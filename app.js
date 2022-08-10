80110f716e5e4b69d10545b8f9ef521e4afc8f03
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-08-10 10:50:29 +0200

    innerhtml

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
+const apiURL = "http://localhost:3000/posts";
+let main = document.getElementById('main');
+let postCont = document.getElementById('postCont');
+let ul = document.createElement('ul')
+const section = `
+<div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Accordion Item #1
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="postCont" class="accordion-body">
+     
+                </div>
+            </div>
+    </div>
+</div>`;
 
-fetch('http://localhost:3000/posts')
-.then(res=> res.json())
-.then(data => {
-    console.log(data)
-})
+
+
+
+        fetch(`${apiURL}`)
+        .then(res=> res.json())
+        .then(data => {
+        data.forEach(data => {
+            let element = document.createElement('li');
+            element.appendChild(
+                document.createTextNode(`${data.title}`)
+                );
+            ul.appendChild(element);
+        });
+        postCont.appendChild(ul);
+    })
+
+    
+
+
+

04fcc6ad216fa167d57f3ef0a3f8d0a71891f622
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-08-10 13:06:05 +0200

    modal

--- app.js
+++ app.js
@@ -1,115 +1,49 @@
 const apiURL = "http://localhost:3000/posts";
-let main = document.getElementById('main');
-let postCont = document.getElementById('postCont');
-let ul = document.createElement('ul');
-
-
-
-
-/* const section = `
-<div class="accordion" id="accordionExample">
-    <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Accordion Item #1
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div id="postCont" class="accordion-body">
-     
-                </div>
-            </div>
-    </div>
-</div>`; */
-
-
-
-
-      /*   fetch(`${apiURL}`)
-        .then(res=> res.json())
-        .then(data => {
-        data.forEach(data => {
-            let element = document.createElement('h3');
-            element.appendChild(
-                document.createTextNode(`${data.title}`)
-                );
-            ul.appendChild(element);
-
-            let users = document.createElement('h3');
-            users.appendChild(
-                document.createTextNode(`${data.body}`)
-            );
-            ul.appendChild(users);
-        });
-        postCont.appendChild(ul);
-    })  */
-
-
-
-
+const modalBody = document.querySelector(".modal-body")
 
 
 
      fetch(`${apiURL}`)
         .then(res=> res.json())
         .then(data => {
-        data.forEach(data => {
-
+        data.forEach(post => {
+            let divcont = document.createElement('div')
             //userID
-            let users = document.createElement('h5');
+            /*let users = document.createElement('h3');
             users.appendChild(
-                document.createTextNode(`${data.userId}`)
+                document.createTextNode(`${post.userId}`)
             );
-            ul.appendChild(users);
+            divcont.appendChild(users);
 
             //id
             let ids = document.createElement('h5');
             ids.appendChild(
-                document.createTextNode(`${data.id}`)
+                document.createTextNode(`${post.id}`)
             );
-            ul.appendChild(ids);
+            divcont.appendChild(ids);*/
 
             //title
 
             let element = document.createElement('h3');
             element.appendChild(
-                document.createTextNode(`${data.title}`)
+                document.createTextNode(`${post.title}`)
+                
                 );
-            ul.appendChild(element);
+            divcont.setAttribute("data-bs-toggle","modal")
+            divcont.setAttribute("data-bs-target","#staticBackdrop")
+            divcont.appendChild(element);
 
             //body
 
-            let content = document.createElement('h3');
+            let content = document.createElement('p');
             content.appendChild(
-                document.createTextNode(`${data.body}`)
+                document.createTextNode(`${post.body}`)
             );
-            ul.appendChild(content);
+            divcont.appendChild(content);
+            main.appendChild(divcont);
         });
-        postCont.appendChild(ul);
+        
     }) 
 
 
-
-    
-
-
-
-
-
-
-/*     
-fetch(`${apiURL}`)
-.then(res => res.json())
-.then(data => {
-    data.forEach(data => {
-        let bucle = document.createElement('div');
-        bucle.appendChild(
-            document.createTextNode(`${data.title}`)
-        );
-        div.appendChild(bucle);
-    });
-    postCont.appendChild(div);
-})
-     */
-
-
+const usersApi = "http://localhost:3000/users";
